5778ac84be8217b3899cdc35d133916b4f733a12
raghulsukumar <ratt0008@student.monash.edu>
2024-09-13 16:05:34 +1000

    Added delete book funstion

--- raghul-library/src/views/AddBookView.vue
+++ raghul-library/src/views/AddBookView.vue
@@ -12,21 +12,31 @@
       </div>
       <button type="submit">Add Book</button>
     </form>
-    <BookList/>
+  </div>
+  
+  <div>
+    <BookList />
+  </div>
+  
+  <div>
+    <h1>Delete Book</h1>
+    <label for="bookName">Book Name:</label>
+    <input type="text" v-model="bookName" id="bookName" required />
+    <button @click="deleteBook">Delete Book</button>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
-import db from '../firebase/init.js'
-import { collection, addDoc } from 'firebase/firestore';
-
+import db from '../firebase/init.js';
+import { collection, addDoc, doc, deleteDoc, getDocs, query, where } from 'firebase/firestore';
 import BookList from '../components/BookList.vue';
 
 export default {
   setup() {
     const isbn = ref('');
     const name = ref('');
+    const bookName = ref('');
 
     const addBook = async () => {
       try {
@@ -40,20 +50,39 @@ export default {
           isbn: isbnNumber,
           name: name.value
         });
-        
+
         isbn.value = '';
         name.value = '';
-        
         alert('Book added successfully!');
       } catch (error) {
         console.error('Error adding book: ', error);
       }
     };
 
+    const deleteBook = async () => {
+      try {
+        const bookQuery = query(collection(db, 'books'), where('name', '==', bookName.value));
+        const querySnapshot = await getDocs(bookQuery);
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach(async (docSnap) => {
+            await deleteDoc(doc(db, 'books', docSnap.id));
+          });
+          bookName.value = '';
+          alert('Book deleted successfully!');
+        } else {
+          alert('Book not found');
+        }
+      } catch (error) {
+        console.error('Error deleting book: ', error);
+      }
+    };
+
     return {
       isbn,
       name,
-      addBook
+      addBook,
+      bookName,
+      deleteBook
     };
   },
   components: {

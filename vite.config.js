b18d94302f79a5b230a5c47679a4ae0922e6618c
raghulsukumar <ratt0008@student.monash.edu>
2024-10-18 13:38:07 +1100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,6 @@ export default defineConfig({
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/raghul-fit5032/raghul-library/'
+    ? '/raghul-fit5032/raghul-fit5032/'
     : '/'
 }
